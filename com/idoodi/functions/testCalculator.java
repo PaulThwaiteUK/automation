728a6d02412611e600e401f723542e5065a5a346
idoodi <paulpthwaite@gmail.com>
2015-05-27 13:37:54 +0100

    New calculator class

---
+++ com/idoodi/functions/testCalculator.java
@@ -0,0 +1,30 @@
+package com.idoodi.functions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testCalculator {
+
+	@Test
+	public void test_multiply() {
+		
+		Calculator calculate = new Calculator();
+		assertEquals("4 * 6 = 24", 24, calculate.multiply(4, 6));
+	}
+	
+	@Test
+	public void test_add() {
+		
+		Calculator calculate = new Calculator();
+		assertEquals("4 + 6 = 10", 10, calculate.add(4, 6));
+	}
+	
+	@Test
+	public void test_add_generateTestFailure() {
+		
+		Calculator calculate = new Calculator();
+		assertEquals("4 * 6 = 24", 11, calculate.add(4, 6));
+	}
+
+}
